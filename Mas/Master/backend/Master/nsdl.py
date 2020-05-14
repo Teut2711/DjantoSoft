06db08c3853dc9ab57b255e002d09b1448672852
XtremeGood <manglavishesh64@gmail.com>
2020-05-14 14:29:37 +0530

    Some Error

--- Mas/Master/backend/Master/nsdl.py
+++ Mas/Master/backend/Master/nsdl.py
@@ -1,14 +1,16 @@
+from django.db import IntegrityError
+
 from Master import models
 
+import logging
 from abc import ABCMeta
-
-import json
 import pickle
 import pandas as pd
 import os
 import pathlib
 from more_itertools.more import split_before
 
+logging.basicConfig(filename="Master/backend/Master/Logs/loggs.log", level=logging.INFO)
 
 PATH = pathlib.Path(__file__).parent
 
@@ -21,12 +23,12 @@ def get_cols(cols_file):
 
 class GetAllInfo:
     TOTAL_ROWS_INPUT_FILE = 0
-    IN_TABLE_DEMATAD = 0  # models.Dematad.objects.count()
-    IN_TABLE_Demathol = 0  # models.Demathol.objects.count()
-    inserts_DEMATAD = 0  # models.Dematad.objects.count() - IN_TABLE_DEMATAD
-    inserts_Demathol = 0  # models.Demathol.objects.count() - IN_TABLE_Demathol
-    updates_DEMATAD = 0
-    updates_Demathol = 0
+    WERE_IN_TABLE_DEMATAD = 0
+    WERE_IN_TABLE_DEMATHOL = 0
+    INSERTS_DEMATAD = 0
+    INSERTS_DEMATHOL = 0
+    UPDATES_DEMATAD = 0
+    UPDATES_DEMATHOL = 0
 
 
 class GetDataFrame:
@@ -40,11 +42,11 @@ class GetDataFrame:
             if len(chunk) <= 1:
                 continue
             yield GetDataFrame.chunk_to_df(chunk, cols)
+
         # from nodeirc by _habnabit
 
     @staticmethod
     def chunk_to_df(chunk, cols):
-
         isin, date = chunk[0].split("##")[1:3]
         chunk = chunk[1:]
         data = [i.split("##")[2:] for i in chunk]
@@ -55,61 +57,114 @@ class GetDataFrame:
         return df
 
 
-class CleanFuncs:
-    @staticmethod
-    def dematad_clean(df):
-        return df.drop_duplicates(
-            subset=["DPID", "CLID"],
-            keep='first')
+# class CleanFuncs:
+#     @staticmethod
+#     def Dematad_clean(df):
+#         return df.drop_duplicates(
+#             subset=["DPID", "CLID"],
+#             keep='first')
 
-    @staticmethod
-    def Demathol_clean(df):
-        return df.drop_duplicates(
-            subset=["DPID", "CLID"],
-            keep='first')
+#     @staticmethod
+#     def Demathol_clean(df):
+#         return df.drop_duplicates(
+#             subset=["DPID", "CLID"],
+#             keep='first')
 
 
 class ProcessDf:
+    # ANCHOR The following class takes in a pandas dataframe and creates/updates the tables
 
-    def processdematad(self, df, cols_dematad, first_time=True):
+    @staticmethod
+    def processDematad( df, cols_Dematad, first_time=True):
         if first_time:
-            models.Dematad.bulk_create(df[cols_dematad].to_dict(
-                orient="records"), ignore_conflicts=True)
-        else:
-            for i in df[cols_dematad].to_dict(orient="records"):
-                created_or_not = models.Dematad.update_or_create(i)[1]
-                if not(created_or_not):
-                    GetAllInfo.updates_DEMATAD += 1
+            for i in (df[cols_Dematad]).to_dict(orient="records"):
+                try:
+                    models.Dematad.objects.create(**i)
+                except IntegrityError:
+                    pass
                 else:
-                    GetAllInfo.inserts_DEMATAD += 1
+                    GetAllInfo.INSERTS_DEMATAD += 1
+        else:
+            for i in df[cols_Dematad].to_dict(orient="records"):
 
-    def processDemathol(self, df, cols_Demathol, first_time=True):
+                try:
+                    DPID = i.pop("DPID")
+                    CLID = i.pop("CLID")
+                    created_or_not = models.Dematad.objects.update_or_create(
+                        DPID=DPID, CLID=CLID, defaults=i)[1]
+                    print(created_or_not)
+                except IntegrityError:
+                    pass
+                else:
+                    if not(created_or_not):
+                        GetAllInfo.UPDATES_DEMATAD += 1
+                    else:
+                        GetAllInfo.UPDATES_DEMATAD += 1
+                    print("Dematad", " :  ", GetAllInfo.UPDATES_DEMATAD)
+
+    @staticmethod
+    def processDemathol( df, cols_Demathol, first_time=True):
+
         if first_time:
-            models.Demathol.bulk_create(df[cols_Demathol].to_dict(
-                orient="records"), ignore_conflicts=True)
+
+            for i in (df[cols_Demathol]).to_dict(orient="records"):
+                try:
+                    models.Demathol.objects.create(**i)
+                except IntegrityError:
+                    pass
+                else:
+                    GetAllInfo.INSERTS_DEMATHOL += 1
         else:
             for i in df[cols_Demathol].to_dict(orient="records"):
-                created_or_not = models.Demathol.update_or_create(i)[1]
-                if not(created_or_not):
-                    GetAllInfo.updates_Demathol += 1
+
+                try:
+                    DPID = i.pop("DPID")
+                    CLID = i.pop("CLID")
+                    created_or_not = models.Demathol.objects.update_or_create(
+                        DPID=DPID, CLID=CLID, defaults=i)[1]
+                except IntegrityError:
+                    pass
                 else:
-                    GetAllInfo.updates_Demathol += 1
+                    if not(created_or_not):
+                        GetAllInfo.UPDATES_DEMATHOL += 1
+                    else:
+                        GetAllInfo.UPDATES_DEMATHOL += 1
+                    print("Demathol", " :  ", GetAllInfo.UPDATES_DEMATHOL)
 
 
 def main(file_obj):
 
+    GetAllInfo.WERE_IN_TABLE_DEMATAD = models.Dematad.objects.count()
+    GetAllInfo.WERE_IN_TABLE_DEMATHOL = models.Dematad.objects.count()
+
     cols_df = get_cols(os.path.join(PATH, 'cols', 'cols.pk'))
-    cols_dematad = get_cols(os.path.join(PATH, 'cols', 'dematad.pk'))
+    cols_Dematad = get_cols(os.path.join(PATH, 'cols', 'Dematad.pk'))
     cols_Demathol = get_cols(os.path.join(PATH, 'cols', 'Demathol.pk'))
-
+    
     if models.Dematad.objects.exists():
-        for df in reversed(GetDataFrame.read_file_obj(file_obj, cols_df)):
-            ProcessDf.processdematad(df, cols_dematad, True)
-            ProcessDf.processDemathol(df, cols_Demathol, True)
+        dematad = True
+    else:      
+        dematad = False
 
-    else:
-        for df in GetDataFrame.read_file_obj(file_obj, cols_df):
-            ProcessDf.processdematad(df, cols_dematad, False)
-            ProcessDf.processDemathol(df, cols_Demathol, False)
-    return json.dumps({key: value for key, value in vars(GetAllInfo)
-            if not key.startswith('__') and not callable(key)})
+    if models.Demathol.objects.exists():
+        demathol = True
+    else:      
+        demathol = False
+          
+    for df in GetDataFrame.read_file_obj(file_obj, cols_df):
+
+
+        if not(dematad):
+             ProcessDf.processDematad(df, cols_Dematad, first_time=True)
+        else:
+            ProcessDf.processDematad(df, cols_Dematad, first_time =False)
+            
+        if not(demathol):
+             ProcessDf.processDemathol(df, cols_Demathol, first_time = True)
+        else:
+            ProcessDf.processDemathol(df, cols_Demathol, first_time = False)
+
+        GetAllInfo.TOTAL_ROWS_INPUT_FILE += df.shape[0]
+    print("Returning")
+    return {key: value for key, value in vars(GetAllInfo)
+            if not key.startswith('__') and not callable(key)}
--- Mas/Master/views.py
+++ Mas/Master/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect, render
-from django.http import HttpRequest, HttpResponse
-# Create your views here.
 
 from . import forms
 
 from .backend.Master import nsdl, cdsl
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+
 
 
 def index(request):
@@ -18,14 +20,14 @@ def nsdlbenpos(request):
         form = forms.NSDLForm(request.POST, request.FILES)
 
         if form.is_valid():
-
-            return HttpResponse("<h1>Logs:  Number of rows {}</h1>".format(
+            print("Here")
+            return JsonResponse(
                 nsdl.main(form.cleaned_data.get('filepath')
                           )
-                ))
+                )
 
         else:
-            return HttpResponse("<h1>Bad File Input</h1>")
+            return HttpResponse("<h1> Bad File Error </h1>")
 
 
     form=forms.NSDLForm()
